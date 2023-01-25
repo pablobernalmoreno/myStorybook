7b2a84023a203c9de2e19b74d801b229c67ee000
pablobernalmoreno <pbm1998@hotmail.com>
2023-01-25 16:06:28 -0500

    Adding documentation and functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,36 +1,92 @@
 import styled, { css } from "styled-components";
 
-const Button = styled.button`
-  font-size: 16px;
+const MyButton = styled.button`
+  position: relative;
+  border: none;
+  font-size: 18px;
   padding: 10px;
   border-radius: 5px;
-  border: none;
+  color: #ffffff;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  overflow: hidden;
+  cursor: pointer;
+  &:after {
+    content: "";
+    display: block;
+    position: absolute;
+    padding-top: 300%;
+    padding-left: 350%;
+    margin-left: -20px !important;
+    margin-top: -120%;
+    opacity: 0;
+    transition: all 0.8s;
+  }
+  &:active:after {
+    padding: 0;
+    margin: 0;
+    opacity: 1;
+    transition: 0s;
+  }
   ${({ type }) => {
     switch (type) {
       case "Primary":
         return css`
           background-color: #457dff;
           color: #ffffff;
+          &:hover {
+            background-color: #3052a1;
+          }
+          &:after {
+            background: #5c8bf7;
+          }
         `;
       case "Secondary":
         return css`
-          background-color: #FFC745;
+          background-color: #ffc745;
           color: #ffffff;
+          &:hover {
+            background-color: #d9a427;
+          }
+          &:after {
+            background: #fac64b;
+          }
         `;
       case "Tertiary":
         return css`
-        background-color: #8C70FF;
-        color: #ffffff;
+          background-color: #8c70ff;
+          color: #ffffff;
+          &:hover {
+            background-color: #6551b8;
+          }
+          &:after {
+            background: #7f6cd4;
+          }
         `;
       default:
         return css`
           background-color: #bfbfbf;
+          &:hover {
+            background-color: #6b6b6b;
+          }
+          &:after {
+            background: #e6e1e1;
+          }
         `;
     }
   }}
 `;
 
-function MyButton({ label, type }) {
-  return <Button type={type}>{label}</Button>;
+function Button({ label, type, customStyles, onClick, ...otherProps }) {
+  return (
+    <MyButton
+      type={type}
+      style={customStyles}
+      onClick={onClick}
+      {...otherProps}
+    >
+      {label}
+    </MyButton>
+  );
 }
-export default MyButton;
+export default Button;
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,31 +1,77 @@
-import MyButton from "../components/Button";
+import { object } from "prop-types";
+import Button from "../components/Button";
 
 export default {
   title: "Components/Button",
-  component: MyButton,
+  component: Button,
+  argTypes: {
+    label: {
+      name: "label",
+      defaultValue: { summary: "Default" },
+      description: "Label for the button",
+      control: { type: "text" },
+      type: { required: true },
+    },
+    type: {
+      name: "type",
+      defaultValue: { summary: "" },
+      description: "Type of button to give styles",
+      options: ["Primary", "Secondary", "Tertiary", "Default"],
+      control: { type: "radio" },
+    },
+    onClick: {
+      name: "onClick",
+      defaultValue: { summary: "() => {}" },
+      description: "Function to be called on click",
+      type: { required: true },
+    },
+    customStyles: {
+      name: "customStyles",
+      defaultValue: {},
+      description: "Custom styles to be added by the user",
+      control: "object",
+    },
+    otherProps: {
+      name: "otherProps",
+      defaultValue: {},
+      description: "Here you can add your own props and they will work",
+    },
+  },
 };
 
-const Template = (args) => <MyButton {...args} />;
+const Template = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   label: "Default",
-}
+  onClick: () => {},
+};
 
 export const Primary = Template.bind({});
 Primary.args = {
   label: "Primary",
   type: "Primary",
+  onClick: () => {},
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   label: "Secondary",
   type: "Secondary",
+  onClick: () => {},
 };
 
 export const Tertiary = Template.bind({});
 Tertiary.args = {
   label: "Tertiary",
   type: "Tertiary",
+  onClick: () => {},
+};
+
+export const Custom = Template.bind({});
+Custom.args = {
+  label: "Custom",
+  type: "Custom",
+  customStyles: { backgroundColor: "red" },
+  onClick: () => {},
 };
